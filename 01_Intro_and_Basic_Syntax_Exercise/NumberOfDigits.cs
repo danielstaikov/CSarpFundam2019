ad5ffac34f4fd2adba832a9c870fd349151c278e
Daniel Staykov <daniel.staikov@gmail.com>
2019-09-23 21:36:39 +0300

    Add files via upload

---
+++ 01_Intro_and_Basic_Syntax_Exercise/NumberOfDigits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfDigits
+{
+    class NumberOfDigits
+    {
+        static void Main(string[] args)
+        {
+            long i = 0;
+            double n;
+            Console.Write("N? ");
+            n = Convert.ToInt64(Console.ReadLine());
+
+            do
+            {
+                n = n / 10;
+                i++;
+            }
+            while (Math.Abs(n) > 1);
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ 01_Intro_and_Basic_Syntax_Exercise/Rest_01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest_01
+{
+    class Rest_01
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int singleNum = 0;
+            singleNum = number % 10;
+            Console.WriteLine(singleNum);
+        }
+    }
+}
